54b32ea778be52002da4ab01dfe37094561de08a
Harsh <harshv@nedbank.co.za>
2024-03-15 23:18:42 +0530

    Initial Commit

---
+++ Carbon/CarbonApp.swift
@@ -0,0 +1,32 @@
+//
+//  CarbonApp.swift
+//  Carbon
+//
+//  Created by Harsh on 15/03/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CarbonApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
